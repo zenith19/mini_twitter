af0065d6e1f3f0989d9ffd00cc29f5b52bbdfb08
zenithit <knotholaze@gmail.com>
2020-08-21 21:04:35 +0600

    add rails middleware

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :v1 do
-  	resources :users,
-  	resources :posts,
-  	resources :comments,
-  	resources :authentications, only: [:create, :show]
+  	resources :users
+  	resources :posts
+  	resources :comments
   end
 end
